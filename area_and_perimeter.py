877ddcdd34bd33deb35adca71074dd844d7fcee8
Ubuntu <ubuntu@ip-172-31-49-223.ec2.internal>
2021-04-23 14:35:52 +0000

    added in area_and_perimeter.py and basic_math.py

---
+++ area_and_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by Jenoe Balote
+# Created on April 2021
+# This program calculates the area and perimeter of a rectangle
+#       with dimensions 5cm x 3cm
+
+
+def main():
+    # this function calculates the area and perimeter
+    print("If a rectangle has the dimensions:")
+    print("5cm x 3cm")
+    print("5*3={}". format(5*3))
+    print("Area is {}cm^2". format (5*3))
+    print("Perimete is {}cm". format(2*(5+3))) 
+
+    
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by Jenoe Balote
+# Created on April 2021
+
+
+
+def main():
+    # this function does basic math
+    print(9+2)
+    print("7-3={}".format(7-3))
+    print("4/2={}". format(4/2))
+    print("4+4/2={}". format(4+4/2))
+    print("3+4*2={}". format(3+4*2))
+    print("5+2^3={}". format(5+2^3))
+    print("5+2**3={}". format(5+2**3))
+    
+
+if __name__ == "__main__":
+    main()
